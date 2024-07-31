9c01d731182071ffac1837188300ecfb04fa6566
Kennedy Dias <kennedy.dias@ifood.com.br>
2024-07-30 23:00:10 -0300

    Updated design system resources

--- app/src/main/java/dev/kennedydias/pocketlist/MainActivity.kt
+++ app/src/main/java/dev/kennedydias/pocketlist/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.kennedydias.pocketlist.ui.theme.PocketListTheme
+import dev.kennedydias.designsystem.theme.PocketListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- infrastructure/designsystem/build.gradle.kts
+++ infrastructure/designsystem/build.gradle.kts
@@ -30,13 +30,22 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
